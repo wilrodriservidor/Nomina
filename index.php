0286d0b50d0a40d3bfc4a1c0cc3a6e40c33f50b1
Wilman Rodriguez <wilrodri@gmail.com>
2026-04-30 20:38:40 +0000

    agregada vista home

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../core/Router.php';
+require_once __DIR__ . '/core/Router.php';
+
 $router = new Router();
-
-require_once __DIR__ . '/../routes/web.php';
-
 $router->run();
